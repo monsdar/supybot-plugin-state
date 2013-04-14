bd2d02286586a29b9d4a9d482912efea593c29e6
monsdar <monsdar@1fd5d812-7899-5ff1-51bd-e6c28d3f50df>
2013-04-14 14:27:40 +0000

    Initial checkin, every basic feature implemented

---
+++ trunk/test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2013, Nils Brinkmann
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class StateTestCase(PluginTestCase):
+    plugins = ('State',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
